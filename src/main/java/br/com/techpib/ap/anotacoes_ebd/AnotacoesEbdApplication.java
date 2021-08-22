0041f60991003cb499205e18cddadbc029bd3999
augustoluiz <augustogu7@yahoo.com.br>
2021-08-21 21:12:58 -0300

    Estrutura inicial do projeto

---
+++ src/main/java/br/com/techpib/ap/anotacoes_ebd/AnotacoesEbdApplication.java
@@ -0,0 +1,13 @@
+package br.com.techpib.ap.anotacoes_ebd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnotacoesEbdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnotacoesEbdApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/techpib/ap/anotacoes_ebd/AnotacoesEbdApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.techpib.ap.anotacoes_ebd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnotacoesEbdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
