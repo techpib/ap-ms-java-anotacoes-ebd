859c3139701e5dd49d6d00ffb0060094521c02de
augustoluiz <augustogu7@yahoo.com.br>
2021-08-30 19:02:39 -0300

    Logs em UsuarioController e AutenticacaoController

--- src/main/java/br/com/techpib/ap/ms_anotacoes_ebd/adapter/inbound/controller/AutenticacaoController.java
+++ src/main/java/br/com/techpib/ap/ms_anotacoes_ebd/adapter/inbound/controller/AutenticacaoController.java
@@ -3,6 +3,8 @@ package br.com.techpib.ap.ms_anotacoes_ebd.adapter.inbound.controller;
 import br.com.techpib.ap.ms_anotacoes_ebd.adapter.configuration.security.TokenService;
 import br.com.techpib.ap.ms_anotacoes_ebd.core.data.dto.TokenDto;
 import br.com.techpib.ap.ms_anotacoes_ebd.core.data.form.LoginForm;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -15,10 +17,13 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
+import java.util.Date;
 
 @RestController
 @RequestMapping("/ap-anotacoes-ebd/auth")
 public class AutenticacaoController {
+
+    private static final Logger log = LogManager.getLogger(AutenticacaoController.class);
 
     @Autowired
     private AuthenticationManager authenticationManager;
@@ -28,12 +33,16 @@ public class AutenticacaoController {
 
     @PostMapping
     public ResponseEntity<TokenDto> autenticar(@RequestBody @Valid LoginForm form){
+        log.info("[POST] - autenticar, LoginForm: {}, data: {}", form, new Date());
         UsernamePasswordAuthenticationToken dadosLogin = form.converter();
         try {
             Authentication authentication = authenticationManager.authenticate(dadosLogin);
             String token = tokenService.gerarToken(authentication);
-            return ResponseEntity.ok(new TokenDto(token, "Bearer"));
+            var tokenDto = new TokenDto(token, "Bearer");
+            log.info("[POST] - autenticar, Token gerado com sucesso! LoginForm: {}, token: {}, data: {}", form, tokenDto, new Date());
+            return ResponseEntity.ok(tokenDto);
         } catch (AuthenticationException e){
+            log.error("[POST] - autenticar, Erro na geracao do Token! LoginForm: {}, data: {}", form, new Date());
             return ResponseEntity.badRequest().build();
         }
     }
--- src/main/java/br/com/techpib/ap/ms_anotacoes_ebd/adapter/inbound/controller/UsuarioController.java
+++ src/main/java/br/com/techpib/ap/ms_anotacoes_ebd/adapter/inbound/controller/UsuarioController.java
@@ -7,7 +7,9 @@ import br.com.techpib.ap.ms_anotacoes_ebd.core.data.form.UsuarioSenhaForm;
 import br.com.techpib.ap.ms_anotacoes_ebd.core.data.mapper.UsuarioMapper;
 import br.com.techpib.ap.ms_anotacoes_ebd.core.services.interfaces.UsuarioService;
 import br.com.techpib.ap.ms_anotacoes_ebd.core.utils.EncoderMD5;
-import lombok.extern.slf4j.Slf4j;
+
+import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.LogManager;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.http.HttpStatus;
@@ -17,11 +19,13 @@ import org.springframework.web.bind.annotation.*;
 import javax.transaction.Transactional;
 import javax.validation.Valid;
 import java.security.NoSuchAlgorithmException;
+import java.util.Date;
 
-@Slf4j
 @RestController
 @RequestMapping(value = "/ap-anotacoes-ebd/usuario")
 public class UsuarioController {
+
+    private static final Logger log = LogManager.getLogger(UsuarioController.class);
 
     @Autowired
     private UsuarioService usuarioService;
@@ -33,8 +37,12 @@ public class UsuarioController {
     @Transactional
     @CacheEvict(value = "listaUsuarios", allEntries = true)
     public ResponseEntity<UsuarioDTO> save(@RequestBody @Valid UsuarioForm usuarioForm) throws NoSuchAlgorithmException {
-        if(UsuarioUtils.usuarioExiste(usuarioService, usuarioForm.getEmail()))
+        log.info("[POST] - save, usuarioForm: {}, data: {}", usuarioForm, new Date());
+
+        if(UsuarioUtils.usuarioExiste(usuarioService, usuarioForm.getEmail())){
+            log.error("[POST] - save, Usuario nao existe na base! usuarioForm: {}, data: {}", usuarioForm, new Date());
             return ResponseEntity.status(HttpStatus.FORBIDDEN).build();
+        }
         return new ResponseEntity<>(usuarioMapper.converteParaDTO(usuarioService.save(usuarioMapper.converteParaEntity(usuarioForm))), HttpStatus.CREATED);
     }
 
@@ -42,8 +50,12 @@ public class UsuarioController {
     @Transactional
     @CacheEvict(value = "listaUsuarios", allEntries = true)
     public ResponseEntity<UsuarioDTO> updateSenha(@RequestBody @Valid UsuarioSenhaForm usuarioSenhaForm) throws NoSuchAlgorithmException {
-        if(!UsuarioUtils.usuarioESenhaValido(usuarioService, usuarioSenhaForm.getEmail(), EncoderMD5.encodeToMD5(usuarioSenhaForm.getSenhaAtual())) || !novaSenhaValida(usuarioSenhaForm.getSenhaAtual(), usuarioSenhaForm.getSenhaNova()))
+        log.info("[POST] - updateSenha, usuarioForm: {}, data: {}", usuarioSenhaForm, new Date());
+
+        if (!UsuarioUtils.usuarioESenhaValido(usuarioService, usuarioSenhaForm.getEmail(), EncoderMD5.encodeToMD5(usuarioSenhaForm.getSenhaAtual())) || !novaSenhaValida(usuarioSenhaForm.getSenhaAtual(), usuarioSenhaForm.getSenhaNova())){
+            log.error("[POST] - updateSenha, Usuario e/ou senha invalidos! usuarioSenhaForm: {}, data: {}", usuarioSenhaForm, new Date());
             return ResponseEntity.status(HttpStatus.FORBIDDEN).build();
+        }
 
         var usuario = usuarioService.findUsuarioByEmail(usuarioSenhaForm.getEmail()).get();
         usuario.setSenha(usuarioSenhaForm.getSenhaNova());
